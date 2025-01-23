ba0e8b41dcd3852efdaaa6fe1220761494d546aa
Ronaldo Biagini <biagini@milanleiloes.com.br>
2025-01-23 10:03:44 -0300

    approval basis tested

--- src/main/java/com/biaginitests/dslist/repositories/GameRepository.java
+++ src/main/java/com/biaginitests/dslist/repositories/GameRepository.java
@@ -11,7 +11,7 @@ import com.biaginitests.dslist.projections.GameMinProjection;
 public interface GameRepository extends JpaRepository<Game, Long> {
     //O comando nativeQuery = true indica que a query está em sql, não jpql, que é o padrão
     @Query(nativeQuery = true, value = """
-		SELECT games.id AS id, games.title AS title, games.game_year AS `year`, games.img_url AS imgUrl,
+		SELECT games.id AS id, games.title AS title, games.game_year AS gameYear, games.img_url AS imgUrl,
 		games.short_description AS shortDescription, belonging.position
 		FROM games
 		INNER JOIN belonging ON games.id = belonging.game_id
